1c874ed3d0c0960afdfb347481c165ba3076147e
lsa <marceloparavisi>
2017-08-29 16:07:27 -0300

    ignoring some folders

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lsa/workspace3/src
-# Build directory: /home/lsa/workspace3/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
-subdirs(tortoisebot)
