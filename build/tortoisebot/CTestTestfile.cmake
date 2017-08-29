1355363eba002b1f871ab251cde69bd3c9ade424
lsa <marceloparavisi>
2017-08-29 15:55:02 -0300

    added all files

---
+++ build/tortoisebot/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/lsa/workspace3/src/tortoisebot
+# Build directory: /home/lsa/workspace3/build/tortoisebot
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
